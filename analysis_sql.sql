b4ccde62cabdbc9aa3e05eab510256d6515a742c
talita <122838663+TalitaURZ@users.noreply.github.com>
2023-03-27 15:21:15 -0400

    Update analysis_sql.sql

--- analysis_sql.sql
+++ analysis_sql.sql
@@ -3,10 +3,10 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries s ON e.emp_no = s.emp_no;
 
--- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
-SELECT first_name, last_name, hire_date
+-- 2. List the first name, last name, and hide date for the employees who were hired in 1986.
+SELECT first_name, last_name, hide_date
 FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+WHERE hide_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
